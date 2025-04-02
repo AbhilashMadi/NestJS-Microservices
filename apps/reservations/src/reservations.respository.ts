6ce681c41af5d07e6818756d804ba2e40da22047
Abhilash Kumar <madiabhilash1563@gmail.com>
2025-04-02 08:21:31 +0530

    in progress...

--- apps/reservations/src/reservations.respository.ts
+++ apps/reservations/src/reservations.respository.ts
@@ -1,9 +1,8 @@
-import { Inject, Injectable, Logger } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
 import { AbstractRepository } from '~app/common'
 import { ReservationDocument } from './models/reservation.schema'
-import { ReservationsModule } from './reservations.module'
 
 @Injectable()
 export class ReservationsRepository extends AbstractRepository<ReservationDocument> {
